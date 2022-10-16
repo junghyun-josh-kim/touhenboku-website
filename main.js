01921ed226ffcae502fd491c5cd81fa598c03cb8
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-10-16 16:51:32 -0400

    CSS: Tomo

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Landing page sliding banner
+
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
@@ -13,4 +15,12 @@ btn2.addEventListener('click', () => {
 
 btn3.addEventListener('click', () => {
   container.style.transform = 'translate(-200vw)';
+});
+
+// Navbar toggle button for small screen
+
+const navBarMenu = document.querySelector('.navbar__menu');
+const navBarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navBarToggleBtn.addEventListener('click', () => {
+  navBarMenu.classList.toggle('open');
 });
